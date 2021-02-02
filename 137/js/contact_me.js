12e7946fcfed7f9a946d5a925c269284a24d8d09
Sami Ezzerouali <sami.ezzerouali@amadeus.com>
2021-02-02 15:30:12 +0100

    phone

--- 137/js/contact_me.js
+++ 137/js/contact_me.js
@@ -12,7 +12,7 @@ $(function() {
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
-      var messagePayload = name+" : "+message+'\n'+"email : "+email;
+      var messagePayload = name+" : \n"+message+'\n'+"email : "+email+'\n'+"phone : "+phone;
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -45,12 +45,14 @@ $(function() {
           $('#contactForm').trigger("reset");
         },
         error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
